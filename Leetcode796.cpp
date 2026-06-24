1171d76f245892cde43d3a843f13353ed749cbf1
Anuj006 <asen65287@gmail.com>
2026-06-24 21:41:57 +0530

    Added Leetcode540 (Rotate String)

---
+++ Leetcode796.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {          //This is an Easy Problem to Rotate String. Solved By String Method
+        if((s + s).contains(goal) == true){             //I Simply concatenated the String and checked if it contains the goal
+            return true;                                //Cause when you rotate the string we get our goal inside it
+        }
+        return false;
+    }
+};
+//Commited by Anuj Sen
