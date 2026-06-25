c3640aa2caed0bb2b79ca503ce552f0e89617b75
Anuj006 <asen65287@gmail.com>
2026-06-25 22:29:45 +0530

    Added Leetcode28 (First Occurrence in a String)

---
+++ Leetcode28.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {                    //Easy Problem to Find the Index of the First Occurrence in a String. Solved By String Method
+        return haystack.find(needle);
+    }
+};
+//Commited by Anuj Sen
