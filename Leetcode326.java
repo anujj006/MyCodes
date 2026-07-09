564cbd6a99bf348294c6d229a80147a1fdde21fe
Anuj006 <asen65287@gmail.com>
2026-07-09 21:43:08 +0530

    Added Leetcode326 (Power Of 3)

---
+++ Leetcode326.java
@@ -0,0 +1,10 @@
+package MyCodes;
+
+public class Leetcode326 {
+    for(int i = 0; i < 31; i++){
+            if(n == pow(3, i)){
+                return true;
+            }
+        }
+        return false;
+}
