b172fe5c72cb7069460bbdc61ec5c688d1c996a8
Anuj006 <asen65287@gmail.com>
2026-07-10 19:53:43 +0530

    Added Leetcode977 (Square Of Each Element)

--- Leetcode977.cpp
+++ Leetcode977.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = pow(nums[i], 2);
+    vector<int> sortedSquares(vector<int>& nums) {               //this is a easy problem of leetcode
+        for(int i = 0; i < nums.size(); i++){                    //for loop
+            nums[i] = pow(nums[i], 2);                           //square
         }
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());                          //sort   
         return nums;
     }
 };
